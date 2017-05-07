d5eb1634175cfc4ad7bc1c43603fb25c6cf80c3f
Jason <rhuidean06@gmail.com>
2017-05-06 22:01:04 -0500

    add simple short rate model

---
+++ short_rate.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class Short_rate(object):
+	'''Class model to a constant short rate object
+
+	Parameters
+	==========
+	name : string
+		name of the object
+
+	rate : float
+		positive, constant short rate
+
+	Methods
+	=======
+	getDiscountFactors :
+		returns discount factors for given list/array
+		of dates/times (as year fractions)
+	'''
+
+	def __init__(self, name, rate):
+		self.name = name
+		self.rate = rate
+
+	def getDiscountFactors(self,time_list):
+		time_list = np.array(time_list)
+		return np.exp(-self.rate*time_list)
+	
+sr = Short_rate('r',0.05)
+print "{}{}".format(sr.name,sr.getDiscountFactors([0,1,2,]))
